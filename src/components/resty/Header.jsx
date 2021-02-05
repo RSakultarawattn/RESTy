ed3e356affb470ad275d6a4dd7aaec249d5e59d0
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-05 11:07:13 -0800

    history component built

---
+++ src/components/resty/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <header className={styles.Header}>
+      <h1>
+                INVESTy in RESTy, it's the BESTy!
+      </h1>
+    </header>
+  );
+};
+
+
+
+export default Header;
+
